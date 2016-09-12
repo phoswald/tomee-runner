b85fea051f6cc9e4d7110f9ccad0348c69aed2cf
philip <philip.oswald@gmail.com>
2016-09-12 22:29:41 +0200

    Refactored to use java.nio instead of java.io

--- src/main/java/phoswald/tomee/runner/Main.java
+++ src/main/java/phoswald/tomee/runner/Main.java
@@ -1,8 +1,8 @@
 package phoswald.tomee.runner;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.tomee.embedded.Configuration;
 import org.apache.tomee.embedded.Container;
@@ -16,21 +16,19 @@ public class Main {
             System.out.println("Usage: java -jar tomee-runner.jar <webapp.war>");
             return;
         }
-        File webappFile = new File(args[0]);
-        
+        Path webappFile = Paths.get(args[0]);
         int port = Integer.parseInt(System.getProperty("runner.port", "8080"));
         String context = System.getProperty("runner.context", "");
-
         Path tomcatDir = Files.createTempDirectory("runner-tomee");
         Path webappDir = Files.createTempDirectory("runner-webapp");
-        File webappCopy = new File(webappDir.toFile(), webappFile.getName());
+        Path webappCopy = webappDir.resolve(webappFile.getFileName());
         System.out.println("Using Webapp:          " + webappFile);
         System.out.println("Using Tomcat temp dir: " + tomcatDir);
         System.out.println("Using Webapp temp dir: " + webappDir);
         System.out.println("Using Webapp copy:     " + webappCopy);
         System.out.println("Serving:               http://localhost:" + port + "/" + context);
 
-        Files.copy(webappFile.toPath(), webappCopy.toPath());
+        Files.copy(webappFile, webappCopy);
         //tomcatDir.toFile().deleteOnExit();
         //webappDir.toFile().deleteOnExit();
 
@@ -42,7 +40,7 @@ public class Main {
         Container container = new Container();
         container.setup(configuration);
         container.start();
-        container.deploy(context, webappCopy, true);
+        container.deploy(context, webappCopy.toFile(), true);
         //System.out.println("*** Started.");
 
         // Daemon.main(() -> run(container), () -> stop(container), () -> done());
